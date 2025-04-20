0400a74be25fa2d5f6ab50f66860caa8987a179f
marcelo1037 <marcelo_1037@hotmail.com>
2025-04-19 22:10:54 -0300

    logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,43 +1,66 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
+#include <string.h>
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+        // Dados do Brasil
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
+    char Pais[30] = "Brasil";
+    char Carta[30] = "A01";
+    char Estado[30] = "Pernambuco";
+    float Populacao = 211.1;
+    float Area = 8.510000;
+    float Pib = 2.1740;
+    int PontosTuristicos = 335;
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
+        // Dados da Argentina
 
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
+    char Pais1[30] = "Argentina";
+    char Carta1[30] = "A02";
+    char Estado1[30] = "Buenos Aires";
+    float Populacao1 = 46.7;
+    float Area1 = 2.780000;
+    float Pib1 = 646.1;
+    int PontosTuristicos1 = 220;
 
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
+    char escolha[30];
 
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    printf("Digite o país? (Brasil ou Argentina): \n");
+    scanf("%s", escolha);
+
+    if (strcmp(escolha, "Brasil") == 0) {
+        printf("Carta: %s\n", Carta);
+    } else if (strcmp(escolha, "Argentina") == 0) {
+        printf("Carta: %s\n", Carta1);
+    } else {
+        printf("País inválido.\n");
+    }
+
+        // Comparações
+        
+    if (Populacao >= Populacao1) {
+        printf("A população do Brasil é de 211,1 milhões de habitantes.\n");
+    } else {
+        printf("A população da Argentina é de 46,7 milhões de habitantes.\n");
+    }
+
+    if (Area != Area1) {
+        printf("A extensão do Brasil é de 8.510.000 km²\n");
+    } else {
+        printf("A extensão da Argentina é de 2.780.000 km²\n");
+    }
+
+    if (Pib > Pib1) {
+        printf("O PIB do Brasil é de 2,174 trilhões USD\n");
+    } else {
+        printf("O PIB da Argentina é de 646,1 bilhões USD\n");
+    }
+
+    if (Area > Area1) {
+        printf("O Brasil é o vencedor por sua extensão e seu PIB!\n");
+    } else {
+        printf("A Argentina é a vencedora por sua extensão e seu PIB!\n");
+    }
 
     return 0;
 }
